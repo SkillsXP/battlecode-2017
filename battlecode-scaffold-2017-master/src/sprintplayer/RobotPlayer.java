9051d183f384cc7853c73ae7ed224cfb841ad9b6
Nandha Venkatesh <TheAwesomeTreecko@users.noreply.github.com>
2017-01-17 23:06:57 -0800

    Update RobotPlayer.java

--- battlecode-scaffold-2017-master/src/sprintplayer/RobotPlayer.java
+++ battlecode-scaffold-2017-master/src/sprintplayer/RobotPlayer.java
@@ -161,4 +161,91 @@ public strictfp class RobotPlayer {
 		//Searches for a specific type of robot, presumably an archon
 		return false;
 	}
+		
+	static boolean tryMove(Direction dir, float degreeOffset, int checksPerSide) throws GameActionException {
+        // First, try intended direction
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            return true;
+        }
+
+        // Now try a bunch of similar angles
+        boolean moved = false;
+        int currentCheck = 1;
+
+        while(currentCheck<=checksPerSide) {
+            // Try the offset of the left side
+            if(rc.canMove(dir.rotateLeftDegrees(degreeOffset*currentCheck))) {
+                rc.move(dir.rotateLeftDegrees(degreeOffset*currentCheck));
+                return true;
+            }
+            // Try the offset on the right side
+            if(rc.canMove(dir.rotateRightDegrees(degreeOffset*currentCheck))) {
+                rc.move(dir.rotateRightDegrees(degreeOffset*currentCheck));
+                return true;
+            }
+            // No move performed, try slightly further
+            currentCheck++;
+        }
+
+        // A move never happened, so return false.
+        return false;
+   	}
+	
+	 static boolean willCollideWithMe(BulletInfo bullet) {
+	        MapLocation myLocation = rc.getLocation();
+
+	        // Get relevant bullet information
+	        Direction propagationDirection = bullet.dir;
+	        MapLocation bulletLocation = bullet.location;
+
+	        // Calculate bullet relations to this robot
+	        Direction directionToRobot = bulletLocation.directionTo(myLocation);
+	        float distToRobot = bulletLocation.distanceTo(myLocation);
+	        float theta = propagationDirection.radiansBetween(directionToRobot);
+
+	        // If theta > 90 degrees, then the bullet is traveling away from us and we can break early
+	        if (Math.abs(theta) > Math.PI/2) {
+	            return false;
+	        }
+
+	        // distToRobot is our hypotenuse, theta is our angle, and we want to know this length of the opposite leg.
+	        // This is the distance of a line that goes from myLocation and intersects perpendicularly with propagationDirection.
+	        // This corresponds to the smallest radius circle centered at our location that would intersect with the
+	        // line that is the path of the bullet.
+	        float perpendicularDist = (float)Math.abs(distToRobot * Math.sin(theta)); // soh cah toa :)
+
+	        return (perpendicularDist <= rc.getType().bodyRadius);
+	}
+
+	static boolean tryMove(Direction dir) throws GameActionException {
+        return tryMove(dir,20,3);
+   	}
+	
+	
+	private static boolean trySideStep(BulletInfo bullet) throws GameActionException{
+		Direction towards = bullet.getDir();
+		MapLocation leftGoal = rc.getLocation().add(towards.rotateLeftDegrees(90));
+		MapLocation rightGoal = rc.getLocation().add(towards.rotateRightDegrees(90));
+		return tryMove(towards.rotateRightDegrees(90)) || tryMove(towards.rotateLeftDegrees(90));
+	}
+	
+	private static void dodge() throws GameActionException{
+		BulletInfo[] bullets = rc.senseNearbyBullets();
+		for (BulletInfo bi: bullets){
+			if(willCollideWithMe(bi)){
+				trySideStep(bi);
+			}
+		}
+	}
+	
+	private static void orbit(MapLocation center, float radians){
+		float radius = rc.getLocation().distanceTo(center); //distance from center to robot
+		float circumference = (float) (Math.PI*2*radius);
+		float distance = (float) (radians/(2*Math.PI)*circumference);
+		rc.
+		MapLocation loc; //new location
+	    //logic: go to perspective of tree in the middle. Make it turn towards new location. get new location (radius distance away). move archon to that location.
+	}
+}
 }
